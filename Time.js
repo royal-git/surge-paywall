a28496c9ee29410308a974e2ac805513ea30082c
Royal Thomas <royal.thomas@ucdconnect.ie>
2022-12-02 23:43:15 +0000

    Removing randomness

--- Time.js
+++ Time.js
@@ -1,15 +1,2 @@
-let body = $response.body
+
 alert("hello, world")
-if (/<\/html>|<\/body>/.test(body)) {
-  body = body.replace('</body>', `
-
-<script>
-const elecJSPack = function(){let body = document.querySelector('body');
-  if (body)
-    body.setAttribute('style', 'position:relative !important;');
-$done()}()</script></body>`)
-
-  console.log('Success')
-}
-
-$done({ body })
